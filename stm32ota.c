8117a6196d28f33f496324e14a26609d99eeed53
Bastian de Byl <bastian@debyl.io>
2025-04-11 22:14:26 -0400

    working build

--- stm32ota.c
+++ stm32ota.c
@@ -17,8 +17,7 @@
  * @return char XOR of all bytes in load_address
  */
 static char _stm32_load_address_xor(STM32_LoadAddress_t *load_address) {
-  return load_address->high_byte ^ load_address->mid_high_byte ^
-         load_address->mid_low_byte ^ load_address->low_byte;
+  return load_address->high_byte ^ load_address->mid_high_byte ^ load_address->mid_low_byte ^ load_address->low_byte;
 }
 
 /**
@@ -32,8 +31,8 @@ static char _stm32_load_address_xor(STM32_LoadAddress_t *load_address) {
 static esp_err_t _stm32_await_rx(STM32_OTA_t *stm32_ota, size_t expected_size) {
   // Setup awaiting expected response size in the UART buffer prior to
   // continuing, this is a blocking call and could be improved later
-  uint16_t timer = 0;
-  size_t read_size = 0;
+  uint16_t timer     = 0;
+  size_t   read_size = 0;
 
   // Loop to check against timeout or if we got the expected size
   esp_err_t uart_err = ESP_OK;
@@ -69,12 +68,10 @@ static esp_err_t _stm32_await_rx(STM32_OTA_t *stm32_ota, size_t expected_size) {
  * @param response_size
  * @return esp_err_t
  */
-static esp_err_t _stm32_write_bytes(STM32_OTA_t *stm32_ota,
-                                    const char *write_bytes, size_t write_size,
+static esp_err_t _stm32_write_bytes(STM32_OTA_t *stm32_ota, const char *write_bytes, size_t write_size,
                                     size_t response_size) {
   // Send initial UART data and ensure response size is what we expect
-  const int bytes_written =
-      uart_write_bytes(stm32_ota->uart_port, write_bytes, write_size);
+  const int bytes_written = uart_write_bytes(stm32_ota->uart_port, write_bytes, write_size);
   if (bytes_written != write_size) {
     return ESP_ERR_INVALID_SIZE;
   }
@@ -88,8 +85,7 @@ static esp_err_t _stm32_write_bytes(STM32_OTA_t *stm32_ota,
   uint8_t *bytes_read = (uint8_t *)calloc(response_size, sizeof(uint8_t));
 
   // Read back data
-  const int read_size = uart_read_bytes(stm32_ota->uart_port, bytes_read,
-                                        read_size, 1000 / portTICK_PERIOD_MS);
+  const int read_size = uart_read_bytes(stm32_ota->uart_port, bytes_read, response_size, 1000 / portTICK_PERIOD_MS);
 
   // Ensure we got an acknowledgement or there is more than 0 response data
   if (bytes_read[0] != STM32_UART_ACK || read_size == 0) {
@@ -103,31 +99,27 @@ static esp_err_t _stm32_write_bytes(STM32_OTA_t *stm32_ota,
 
 esp_err_t stm32_init(STM32_OTA_t *stm32_ota) {
   // Check for null pointer
-  if (stm32_ota == NULL)
+  if (stm32_ota == NULL) {
     return ESP_ERR_INVALID_ARG;
+  }
 
   // Setup the UART driver
-  STM32_ERROR_CHECK(uart_driver_install(
-      stm32_ota->uart_port, stm32_ota->uart_rx_buffer_size,
-      stm32_ota->uart_tx_buffer_size, stm32_ota->uart_queue_size,
-      stm32_ota->uart_queue, stm32_ota->uart_intr_alloc_flags));
+  STM32_ERROR_CHECK(uart_driver_install(stm32_ota->uart_port, stm32_ota->uart_rx_buffer_size,
+                                        stm32_ota->uart_tx_buffer_size, stm32_ota->uart_queue_size,
+                                        stm32_ota->uart_queue, stm32_ota->uart_intr_alloc_flags));
 
-  STM32_ERROR_CHECK(
-      uart_param_config(stm32_ota->uart_port, stm32_ota->uart_config));
+  STM32_ERROR_CHECK(uart_param_config(stm32_ota->uart_port, stm32_ota->uart_config));
 
-  STM32_ERROR_CHECK(uart_set_pin(stm32_ota->uart_port, stm32_ota->stm_tx_pin,
-                                 stm32_ota->stm_rx_pin, UART_PIN_NO_CHANGE,
+  STM32_ERROR_CHECK(uart_set_pin(stm32_ota->uart_port, stm32_ota->stm_tx_pin, stm32_ota->stm_rx_pin, UART_PIN_NO_CHANGE,
                                  UART_PIN_NO_CHANGE));
 
   // Setup GPIO pin for reset pin ~NRST
-  STM32_ERROR_CHECK(
-      gpio_set_direction(stm32_ota->stm_nrst_pin, GPIO_MODE_OUTPUT));
+  STM32_ERROR_CHECK(gpio_set_direction(stm32_ota->stm_nrst_pin, GPIO_MODE_OUTPUT));
 
   STM32_ERROR_CHECK(gpio_set_level(stm32_ota->stm_nrst_pin, 1));
 
   // Setup GPIO pin for boot0 pin
-  STM32_ERROR_CHECK(
-      gpio_set_direction(stm32_ota->stm_boot0_pin, GPIO_MODE_OUTPUT));
+  STM32_ERROR_CHECK(gpio_set_direction(stm32_ota->stm_boot0_pin, GPIO_MODE_OUTPUT));
 
   STM32_ERROR_CHECK(gpio_set_level(stm32_ota->stm_boot0_pin, STM32_LOW));
 
@@ -136,8 +128,7 @@ esp_err_t stm32_init(STM32_OTA_t *stm32_ota) {
   }
 
   // Setup GPIO pin for boot1 pin if used
-  STM32_ERROR_CHECK(
-      gpio_set_direction(stm32_ota->stm_boot1_pin, GPIO_MODE_OUTPUT));
+  STM32_ERROR_CHECK(gpio_set_direction(stm32_ota->stm_boot1_pin, GPIO_MODE_OUTPUT));
   return gpio_set_level(stm32_ota->stm_boot1_pin, STM32_LOW);
 }
 
@@ -171,43 +162,39 @@ esp_err_t stm32_ota_begin(STM32_OTA_t *stm32_ota) {
 
   // 1. Initial bootloader command
   char cmd_bootloader[] = {0x7F};
-  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota->uart_port, cmd_bootloader, 1, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_bootloader, 1, 1));
 
   // 2. Run the Get command and validate we got expected response size
   // TODO: do something with response information
   char cmd_get[] = {0x00, 0xFF};
-  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota->uart_port, cmd_get, 2, 15));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_get, 2, 15));
 
   // 3. Run the Get Version command and validate we get expected response size
   // TODO: do something with response information
   char cmd_get_version[] = {0x01, 0xFE};
-  STM32_ERROR_CHECK(
-      _stm32_write_bytes(stm32_ota->uart_port, cmd_get_version, 2, 5));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_get_version, 2, 5));
 
   // 4. Run the Get ID command and validate we get expected response size
   // TODO: do something with response information
   char cmd_get_id[] = {0x02, 0xFD};
-  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota->uart_port, cmd_get_id, 2, 5));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_get_id, 2, 5));
 
   // 5.1. Erase all data on the STM32
   // TODO: do something with response information
   char cmd_erase[] = {0x43, 0xBC};
-  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota->uart_port, cmd_erase, 2, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_erase, 2, 1));
 
   // 5.2. Send a global erase once the erase has been acknowledged
   char cmd_erase_global[] = {0xFF, 0x00};
-  STM32_ERROR_CHECK(
-      _stm32_write_bytes(stm32_ota->uart_port, cmd_erase_global, 2, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_erase_global, 2, 1));
 
   // 6.1. Send extended erase (is this necessary?)
   char cmd_extended_erase[] = {0x44, 0xBB};
-  STM32_ERROR_CHECK(
-      _stm32_write_bytes(stm32_ota->uart_port, cmd_extended_erase, 2, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_extended_erase, 2, 1));
 
   // 6.2. Send mass erase special instruction to extended erase with checksum
   char cmd_extended_erase_mass[] = {0xFF, 0xFF, 0x00};
-  STM32_ERROR_CHECK(
-      _stm32_write_bytes(stm32_ota->uart_port, cmd_extended_erase_mass, 3, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_extended_erase_mass, 3, 1));
 
   // Begin procedure is finished, we are ready to start writing to the STM32
   return ESP_OK;
@@ -221,26 +208,21 @@ esp_err_t stm32_ota_end(STM32_OTA_t *stm32_ota) {
   return gpio_set_level(stm32_ota->stm_nrst_pin, STM32_HIGH);
 }
 
-esp_err_t stm32_ota_write_page(STM32_OTA_t *stm32_ota,
-                               STM32_LoadAddress_t *load_address,
-                               const char *ota_data, size_t ota_data_size) {
+esp_err_t stm32_ota_write_page(STM32_OTA_t *stm32_ota, STM32_LoadAddress_t *load_address, const char *ota_data,
+                               size_t ota_data_size) {
   if (ota_data_size > STM32_MAX_PAGE_SIZE) {
     return ESP_ERR_INVALID_SIZE;
   }
 
   char cmd_write[] = {0x31, 0xCE};
-  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota->uart_port, cmd_write, 2, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_write, 2, 1));
 
   // Send the load address with the last byte being an XOR of all bytes combined
   char cmd_load_address[] = {
-      load_address->high_byte,
-      load_address->mid_high_byte,
-      load_address->mid_low_byte,
-      load_address->low_byte,
-      _stm32_load_address_xor(load_address),
+      load_address->high_byte, load_address->mid_high_byte,           load_address->mid_low_byte,
+      load_address->low_byte,  _stm32_load_address_xor(load_address),
   };
-  STM32_ERROR_CHECK(
-      _stm32_write_bytes(stm32_ota->uart_port, cmd_load_address, 5, 1));
+  STM32_ERROR_CHECK(_stm32_write_bytes(stm32_ota, cmd_load_address, 5, 1));
 
   // Send the size of the data to be written
   if (uart_write_bytes(stm32_ota->uart_port, &ota_data_size, 1) != 1) {
@@ -248,8 +230,7 @@ esp_err_t stm32_ota_write_page(STM32_OTA_t *stm32_ota,
   }
 
   // Write the page to STM32 memory
-  if (uart_write_bytes(stm32_ota->uart_port, ota_data, ota_data_size) !=
-      ota_data_size) {
+  if (uart_write_bytes(stm32_ota->uart_port, ota_data, ota_data_size) != ota_data_size) {
     return ESP_ERR_INVALID_SIZE;
   }
 
@@ -263,15 +244,14 @@ esp_err_t stm32_ota_write_page(STM32_OTA_t *stm32_ota,
   }
 
   // Await ACK from STM32
-  esp_err_t err = _stm32_await_rx(stm32_ota->uart_port, 1);
+  esp_err_t err = _stm32_await_rx(stm32_ota, 1);
   if (err != ESP_OK) {
     return err;
   }
 
   // Validate we received the ACK indicating successful memory write operation
-  uint8_t response = 0x00;
-  const int received_size =
-      uart_read_bytes(stm32_ota->uart_port, &response, 1, STM32_UART_TIMEOUT);
+  uint8_t   response      = 0x00;
+  const int received_size = uart_read_bytes(stm32_ota->uart_port, &response, 1, STM32_UART_TIMEOUT);
 
   if (received_size == 0) {
     return ESP_ERR_TIMEOUT;
